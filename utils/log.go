2bcab183f051a21ce84e37e53400f6266d8aa88d
Nguyen DN <nguyen0096@gmail.com>
2021-01-17 09:38:49 +0700

    add filename logging to log util

--- utils/log.go
+++ utils/log.go
@@ -1,8 +1,18 @@
 package utils
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"runtime"
+)
 
 func Info(message string, params ...interface{}) {
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(5, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	fmt.Printf("%s:%d %s\n", file, line, f.Name())
+
 	log.Printf(message, params...)
 }
 
